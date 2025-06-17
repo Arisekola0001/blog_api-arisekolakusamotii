02852c9d75db2bf8d6e1a6dc4e9843b3d321c314
Arisekola0001 <159254842+Arisekola0001@users.noreply.github.com>
2025-06-17 07:41:46 -0700

    Fix: updated server.js for stable PipeOps deploy

--- server.js
+++ server.js
@@ -5,25 +5,20 @@ dotenv.config();
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 8080;
-
-
+const PORT = process.env.PORT || 8000;
 const MONGO_URI = process.env.MONGO_URI;
 
 const server = http.createServer(app);
 
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connection successful');
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log(' MongoDB connection successful');
-  server.listen(PORT, () => {
-    console.log(` Blog API live on http://localhost:${PORT}`);
+    server.listen(PORT, '0.0.0.0', () => {
+      console.log(` Blog API live on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(' MongoDB connection failed:', error.message);
+    process.exit(1); 
   });
-})
-.catch((error) => {
-  console.error('MongoDB connection failed:', error.message);
-  process.exit(1); 
-});
